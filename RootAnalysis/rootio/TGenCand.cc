0e32ed16e306c38e951990585b1d0b25698fa2fa
Urs Langenegger <urs.langenegger@psi.ch>
2015-03-25 11:54:47 +0100

    avoid numerical problems

--- RootAnalysis/rootio/TGenCand.cc
+++ RootAnalysis/rootio/TGenCand.cc
@@ -32,7 +32,7 @@ void TGenCand::dump(int printPt) {
     sprintf(line, "%4d %+6d S%2d mom(%4d,%4d) dau(%5d,%5d) P=%8.3f(%+9.3f,%+9.3f,%+9.3f) v=(%+8.6f,%+8.6f,%+8.6f)", 
 	    fNumber, fID, fStatus, fMom1, fMom2, fDau1, fDau2,
 	    fP.Rho(), 
-	    fP.Perp(), (fP.Perp()>0?fP.Eta():99.), fP.Phi(),
+	    fP.Perp(), (fP.Perp()>0.01?fP.Eta():99.), fP.Phi(),
 	    fV.X(), fV.Y(), fV.Z());
   } else {
     sprintf(line, "%4d %+6d S%2d mom(%4d,%4d) dau(%5d,%5d) p=%8.3f(%+9.3f,%+9.3f,%+9.3f) v=(%+8.6f,%+8.6f,%+8.6f)", 
