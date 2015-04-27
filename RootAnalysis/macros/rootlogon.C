c06c64b017867fa2f878a8a8cf534068192a6d2b
Urs Langenegger <urs.langenegger@psi.ch>
2015-04-27 09:29:24 +0200

    adapt to ROOT6

--- RootAnalysis/macros/rootlogon.C
+++ RootAnalysis/macros/rootlogon.C
@@ -1,23 +1,7 @@
 {
-#include <string>
-  
-  using namespace std;
-  using namespace RooFit;
-  using namespace RooStats;
-
   string version = gSystem->Getenv("VERSION"); 
   
   cout << "Loading rootlibs" << endl;
-  gSystem->Load("libPhysics.so");
-  gSystem->Load("libTree.so");
-  gSystem->Load("libMinuit.so");
-  gSystem->Load("libHtml.so");
-  
-  gSystem->Load("libRooStats.so");
-  gSystem->Load("libRooFit.so");
-  gSystem->Load("libRooFitCore.so");
-  gSystem->Load("libTMVA.so");
-
   gSystem->Load("lib/libAnaClasses.so");
   
   gROOT->Macro("cms-tdr.C");
@@ -35,7 +19,7 @@
   c = (TCanvas*)gROOT->FindObject("c0"); if (c) c->Delete(); c = 0;
   p = (TPad*)gROOT->FindObject("p0"); if (p) p->Delete(); p = 0;
   // --- Create a new canvas.
-  TCanvas c0("c0","--c0--",2303,0,656,700);
+  TCanvas *c0 = new TCanvas("c0","--c0--",2303,0,656,700);
   c0->ToggleEventStatus();
 }
 
