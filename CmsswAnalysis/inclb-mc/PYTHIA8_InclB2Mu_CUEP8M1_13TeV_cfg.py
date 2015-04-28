8cdefc4ed2ad969885774b14e284242cdfeb2ae4
Urs Langenegger <urs.langenegger@psi.ch>
2015-04-28 11:56:06 +0200

    more stats

--- CmsswAnalysis/inclb-mc/PYTHIA8_InclB2Mu_CUEP8M1_13TeV_cfg.py
+++ CmsswAnalysis/inclb-mc/PYTHIA8_InclB2Mu_CUEP8M1_13TeV_cfg.py
@@ -22,7 +22,7 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(20000)
+    input = cms.untracked.int32(50000)
 )
 
 
