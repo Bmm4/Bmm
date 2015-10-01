92e27cb8159f1d51566510a7e2a0dc8277f11f3f
Urs Langenegger <urs.langenegger@psi.ch>
2015-10-01 11:48:23 +0200

    add all pd/triggers

--- CmsswAnalysis/plugins/HFDumpTrigger.cc
+++ CmsswAnalysis/plugins/HFDumpTrigger.cc
@@ -799,22 +799,38 @@ void  HFDumpTrigger::beginRun(const Run &run, const EventSetup &iSetup)
 {
   bool hasChanged;
   validHLTConfig = hltConfig.init(run,iSetup,fHLTProcessName,hasChanged);
-  //  cout << "HFDumpTrigger::beginRun> hltConfig.tableName() = " << hltConfig.tableName() << endl;
-  vector<string> v = hltConfig.datasetNames();
+  cout << hltConfig.tableName() << endl;
+  vector<string> pds = hltConfig.datasetNames();
   
   TDirectory *pDir = gDirectory; 
   gHFFile->cd();
-  TH1D *h1 = new TH1D(Form("pd%d", run.run()), hltConfig.tableName().c_str(), v.size()+1, 0., v.size()+1); 
+  TH1D *h1 = new TH1D(Form("pd_run%d", run.run()), hltConfig.tableName().c_str(), pds.size(), 0., pds.size()); 
   h1->SetDirectory(gHFFile); 
 
-  for (unsigned int i = 0; i < v.size(); ++i) {
-    //    cout << "                           " << v[i] << endl;
-    h1->GetXaxis()->SetBinLabel(i+1, v[i].c_str()); 
+  for (unsigned int i = 0; i < pds.size(); ++i) {
+    cout << "   " << pds[i] << endl;
+    h1->GetXaxis()->SetBinLabel(i+1, pds[i].c_str()); 
   }
 
   h1->Write();
   delete h1; 
 
+
+  for (unsigned int ipd = 0; ipd < pds.size(); ++ipd) {
+    vector<string> pdTriggers = hltConfig.datasetContent(pds[ipd]);
+    cout << "  --> pd: " << pds[ipd] << endl;
+    h1 = new TH1D(Form("triggers_%s_run%d", pds[ipd].c_str(), run.run()), 
+		  Form("triggers_%s_run%d (%s)", pds[ipd].c_str(), run.run(), hltConfig.tableName().c_str()), 
+		  pdTriggers.size(), 0., pdTriggers.size()); 
+    h1->SetDirectory(gHFFile); 
+    for (unsigned int it = 0; it < pdTriggers.size(); ++it) {
+      cout << "           " << pdTriggers[it] << endl;
+      h1->GetXaxis()->SetBinLabel(it+1, pdTriggers[it].c_str()); 
+    }
+    h1->Write();
+    delete h1; 
+  }
+  
   pDir->cd(); 
 }
 
