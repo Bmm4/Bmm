6ca0d25c0c771c5e77ed3f9b0a974d4a5d3c5be3
Urs Langenegger <urs.langenegger@psi.ch>
2015-10-05 11:49:27 +0200

    template files for legacy data (reprocessings)

---
+++ CmsswAnalysis/test/bmm4/jobs/v01/bmm-rereco-Run2012-XXXX.py
@@ -0,0 +1,55 @@
+import os
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HFA")
+
+# ----------------------------------------------------------------------
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+
+# ----------------------------------------------------------------------
+# -- Database configuration
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+# -- Conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+#process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.Geometry.GeometryDB_cff")
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "FT53_V21A_AN6::All"
+
+# ----------------------------------------------------------------------
+# POOLSOURCE
+
+
+
+# ----------------------------------------------------------------------
+rootFileName = "bmm-rereco-Run2012-XXXX.root"
+
+process.tree = cms.EDAnalyzer(
+    "HFTree",
+    verbose        = cms.untracked.int32(0),
+    printFrequency = cms.untracked.int32(100),
+    requireCand    =  cms.untracked.bool(False),
+    fullGenBlock   = cms.untracked.bool(False),
+    fileName       = cms.untracked.string(rootFileName)
+    )
+
+
+# ----------------------------------------------------------------------
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Bmm.CmsswAnalysis.HFRecoStuff_cff")
+process.load("Bmm.CmsswAnalysis.HFBmm_cff")
+process.load("Bmm.CmsswAnalysis.HFPhysicsDeclared_cff")
+
+# ----------------------------------------------------------------------
+process.p = cms.Path(
+    process.recoStuffSequence*
+    process.bmmSequence*
+    process.tree
+)
