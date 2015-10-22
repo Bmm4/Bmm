2d457067266602a7679f66455798255d395a183a
Urs Langenegger <urs.langenegger@psi.ch>
2015-10-22 15:43:48 +0200

    initial set of crab config py files

---
+++ CmsswAnalysis/test/inclb/jobs/v01/crabConfig-c2mu-noevtgen-25ns.py
@@ -0,0 +1,20 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'c2mu-noevtgen-25ns'
+config.General.workArea = 'crab_mc'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'inclb-mc-RunIISpring15DR74-XXXX.py'
+config.JobType.outputFiles = ['inclb-mc-RunIISpring15DR74-XXXX.root']
+
+config.Data.inputDataset = '/InclusivectoMu_cMuonFilter_TuneCUEP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/AODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 20
+config.Data.outLFNDirBase = '/store/user/ursl/inclb/cmsRun/v01/c2mu-noevtgen-25ns'
+config.Data.publication = False
+
+config.Site.storageSite = 'T3_CH_PSI'
