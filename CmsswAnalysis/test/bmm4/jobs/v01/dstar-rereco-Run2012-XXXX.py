cc2c2898b1c8f3b5337dd4ba351798601edd074c
Urs Langenegger <urs.langenegger@psi.ch>
2015-10-28 13:34:38 +0100

    use 5_3 compatible GT loading

--- CmsswAnalysis/test/bmm4/jobs/v01/dstar-rereco-Run2012-XXXX.py
+++ CmsswAnalysis/test/bmm4/jobs/v01/dstar-rereco-Run2012-XXXX.py
@@ -19,8 +19,8 @@ process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 #process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.Geometry.GeometryDB_cff")
 process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 
 process.GlobalTag.globaltag = "FT53_V21A_AN6::All"
