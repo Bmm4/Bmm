797c41992bf3a266227a1147c74855234dec30e2
Urs Langenegger <urs.langenegger@psi.ch>
2015-10-30 13:56:51 +0100

    setHist(dataset*) added

--- RootAnalysis/common/dataset.cc
+++ RootAnalysis/common/dataset.cc
@@ -37,3 +37,21 @@ TH1D*  dataset::getHist(string name, bool clone) {
   return h; 
 }
 
+// ----------------------------------------------------------------------
+TH2D*  dataset::getHist2(string name, bool clone) {
+  if (!fF) return 0;
+  TH2D *h(0); 
+  if (clone) 
+    h = (TH2D*)((TH2D*)(fF->Get(Form("%s", name.c_str()))))->Clone(Form("%s_%s_%d", fFullName.c_str(), name.c_str(), fNclone));
+  else 
+    h = (TH2D*)(fF->Get(Form("%s", name.c_str())));
+
+  cout << fName << ": hist " << h->GetName() << " with entries = " << h->GetEntries() << " from " << fF->GetName() << endl;
+
+  if (!h) return 0; 
+  setHist(h); 
+  if (fColor > -1) setHist(h, fColor, fSymbol, fSize, fWidth); 
+  if (fFillStyle > -1) setFilledHist(h, fColor, fFcolor, fFillStyle, fWidth); 
+  return h; 
+}
+
