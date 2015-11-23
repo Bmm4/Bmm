796d067b077695ed5e7bb00949f528916cbf34a3
Urs Langenegger <urs.langenegger@psi.ch>
2015-11-23 16:24:22 +0100

    (crab) config files for running inclb on complete 2015 datasets

---
+++ CmsswAnalysis/test/inclb/jobs/v01/crabConfig-doubleMuon-2015Dv4.py
@@ -0,0 +1,20 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName     = 'doubleMuon-2015Dv4'
+config.General.workArea        = 'crab_data'
+config.General.transferOutputs = True
+config.General.transferLogs    = True
+
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = 'inclb-prompt-Run2015Dv4-crab.py'
+config.JobType.outputFiles = ['inclb-prompt-Run2015Dv4-XXXX.root']
+
+config.Data.inputDataset  = '/DoubleMuon/Run2015D-PromptReco-v4/AOD'
+config.Data.inputDBS      = 'global'
+config.Data.splitting     = 'LumiBased'
+config.Data.unitsPerJob   = 50
+config.Data.outLFNDirBase = '/store/user/ursl/inclb/cmsRun/v01'
+config.Data.publication   = False
+
+config.Site.storageSite = 'T3_CH_PSI'
---
+++ CmsswAnalysis/test/inclb/jobs/v01/crabConfig-singleMuon-2015Dv3.py
@@ -0,0 +1,20 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName     = 'singleMuon-2015Dv3'
+config.General.workArea        = 'crab_data'
+config.General.transferOutputs = True
+config.General.transferLogs    = True
+
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = 'inclb-prompt-Run2015Dv3-crab.py'
+config.JobType.outputFiles = ['inclb-prompt-Run2015Dv3-XXXX.root']
+
+config.Data.inputDataset  = '/SingleMuon/Run2015D-PromptReco-v3/AOD'
+config.Data.inputDBS      = 'global'
+config.Data.splitting     = 'LumiBased'
+config.Data.unitsPerJob   = 50
+config.Data.outLFNDirBase = '/store/user/ursl/inclb/cmsRun/v01'
+config.Data.publication   = False
+
+config.Site.storageSite = 'T3_CH_PSI'
