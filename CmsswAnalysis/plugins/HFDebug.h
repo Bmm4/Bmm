ac2b453108a4c115a97d868c01efd38832db52fd
Urs Langenegger <urs.langenegger@psi.ch>
2016-04-07 11:35:29 +0200

    work on extrapolating muons

--- CmsswAnalysis/plugins/HFDebug.h
+++ CmsswAnalysis/plugins/HFDebug.h
@@ -1,88 +1,70 @@
 #ifndef GUARD_HFDEBUG_H
 #define GUARD_HFDEBUG_H
 
-// system include files
-#include <memory>
+#include "Bmm/CmsswAnalysis/plugins/HFVirtualDecay.h"
 
-// user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Common/interface/TriggerNames.h"
 
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
+#include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
-#include "DataFormats/HLTReco/interface/TriggerEvent.h"
 
-#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
-
-#include "MagneticField/Engine/interface/MagneticField.h"
-
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
-#include "DataFormats/VertexReco/interface/Vertex.h"
-#include "DataFormats/VertexReco/interface/VertexFwd.h"
-#include "DataFormats/L1Trigger/interface/L1MuonParticle.h" 
+#include "DataFormats/L1Trigger/interface/L1MuonParticle.h"
 #include "DataFormats/L1Trigger/interface/L1MuonParticleFwd.h"
-#include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
-#include "DataFormats/RecoCandidate/interface/RecoChargedCandidateFwd.h"
-
-#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "DataFormats/L1Trigger/interface/L1ParticleMap.h"
+#include "DataFormats/L1Trigger/interface/L1ParticleMapFwd.h"
+#include "DataFormats/HLTReco/interface/TriggerEventWithRefs.h"
+#include "DataFormats/HLTReco/interface/TriggerObject.h"
+#include "DataFormats/HLTReco/interface/TriggerFilterObjectWithRefs.h"
+#include "DataFormats/HLTReco/interface/TriggerTypeDefs.h"
+#include "DataFormats/HLTReco/interface/TriggerEvent.h"
 
 #include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
 #include "HLTrigger/HLTcore/interface/HLTPrescaleProvider.h"
 
+#include "L1Trigger/GlobalTriggerAnalyzer/interface/L1GtUtils.h"
 
-class TFile;
-class TTree;
-class TAna01Event;
-
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
 
 // ----------------------------------------------------------------------
-class HFDebug : public edm::EDAnalyzer {
+class HFDebug : public HFVirtualDecay {
  public:
   explicit HFDebug(const edm::ParameterSet&);
   ~HFDebug();
   
- private:
+ protected:
   virtual void beginJob() ;
   virtual void beginRun(const edm::Run &, const edm::EventSetup &);
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
   virtual void endRun(edm::Run const&, edm::EventSetup const&);
 
+  virtual void dumpConfiguration();
+  double matchTrigger(std::vector<int> &trigIndices, const trigger::TriggerObjectCollection &trigObjs, 
+		      edm::Handle<trigger::TriggerEvent> &triggerEvent, const reco::Muon &mu);
+  TrajectoryStateOnSurface cylExtrapTrkSam(reco::TrackRef track, double rho);
+  TrajectoryStateOnSurface surfExtrapTrkSam(reco::TrackRef track, double z);
+  FreeTrajectoryState freeTrajStateMuon(reco::TrackRef track);
 
-  int           fVerbose;
-  int           fNevt;
   
-  edm::InputTag fTracksLabel;
-  edm::EDGetTokenT<edm::View<reco::Track> > fTokenTrack;
-
-  edm::InputTag fL1MuonsLabel;
-
-  std::string   fHLTProcessName; 
-
   edm::InputTag fTriggerEventLabel;
   edm::EDGetTokenT<trigger::TriggerEvent> fTokenTriggerEvent;
 
   edm::InputTag fHLTResultsLabel;
   edm::EDGetTokenT<edm::TriggerResults> fTokenTriggerResults;
-  
-  HLTPrescaleProvider  fHltPrescaleProvider;
 
+  std::string   fHLTProcessName; 
+
+  std::vector<std::string> fTriggerNames;
+  std::vector<int>         fTriggerIndices;
+
+  edm::ESHandle<Propagator> fPropagatorAlong;
+  edm::ESHandle<Propagator> fPropagatorOpposite;
+  
   HLTConfigProvider fHltConfig;
   bool              fValidHLTConfig;
-
-  edm::Handle<edm::View<reco::Track> > fTracksHandle;
-
-  // new setup
-  edm::InputTag                                          fL1MuonsNewLabel,
-                                                         fL2MuonsNewLabel,
-                                                         fL3MuonsNewLabel;
-  edm::EDGetTokenT<l1extra::L1MuonParticleCollection>    l1extramuToken_;
-  edm::EDGetTokenT<reco::RecoChargedCandidateCollection> MuCandTag2Token_, MuCandTag3Token_;
-
 
 };
 
