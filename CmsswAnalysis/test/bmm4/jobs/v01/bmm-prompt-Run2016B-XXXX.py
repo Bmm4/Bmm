256460178c2181a009c271b408988a3536dcfdb9
Urs Langenegger <urs.langenegger@psi.ch>
2016-05-24 15:18:28 +0200

    updates

--- CmsswAnalysis/test/bmm4/jobs/v01/bmm-prompt-Run2016B-XXXX.py
+++ CmsswAnalysis/test/bmm4/jobs/v01/bmm-prompt-Run2016B-XXXX.py
@@ -12,8 +12,9 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 # ----------------------------------------------------------------------
 # -- Database configuration
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
+#process.load("CondCore.DBCommon.CondDBCommon_cfi")
+#process.load("CondCore.DBCommon.CondDBSetup_cfi")
+process.load("CondCore.CondDB.CondDB_cfi")
 # -- Conditions
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 #process.load("Configuration.StandardSequences.Geometry_cff")
