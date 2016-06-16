f917128e4e18bc7bf8c636afb5d8ba1c73e2232f
Urs Langenegger <urs.langenegger@psi.ch>
2016-06-16 21:23:00 +0200

    start with TIS analysis

--- RootAnalysis/macros/plotWork.cc
+++ RootAnalysis/macros/plotWork.cc
@@ -25,54 +25,18 @@ using namespace std;
 
 // ----------------------------------------------------------------------
 plotWork::plotWork(string dir, string files, string cuts, string setup): plotClass(dir, files, cuts, setup) {
-  loadFiles(files);
+  plotClass::loadFiles(files);
+  plotWork::loadFiles(files);
 
-  if (setup == "") {
-    fHistFileName = Form("%s/plotWork.root", dir.c_str());
-  } else {
-    fHistFileName = Form("%s/plotWork-%s.root", dir.c_str(), setup.c_str());
-  }
+  changeSetup(dir, "plotWork", setup);
 
-  fTexFileName = fHistFileName;
-  replaceAll(fTexFileName, ".root", ".tex");
-  system(Form("/bin/rm -f %s", fTexFileName.c_str()));
-  fTEX.open(fTexFileName.c_str(), ios::app);
+  // -- initialize cuts
+  string cutfile = Form("%s/%s", dir.c_str(), cuts.c_str());
+  cout << "===> Reading cuts from " << cutfile << endl;
+  readCuts(cutfile);
+  fNchan = fCuts.size();
 
-
-  if (cuts == "") {
-    cout << "==> no cuts provided!?" << endl;
-  } else {
-    cout << Form("==> reading cuts/%s", cuts.c_str()) << endl;
-    readCuts(Form("cuts/%s", cuts.c_str()));
-    printCuts(cout);
-  }
-
-  fAnaCuts.addCut("fGoodHLT", "HLT", fGoodHLT);
-  fAnaCuts.addCut("fGoodMuonsID", "lepton ID", fGoodMuonsID);
-  fAnaCuts.addCut("fGoodMuonsPt", "p_{T,#mu} [GeV]", fGoodMuonsPt);
-  fAnaCuts.addCut("fGoodMuonsEta", "#eta_{#mu}", fGoodMuonsEta);
-  fAnaCuts.addCut("fGoodTracks", "good tracks", fGoodTracks);
-  fAnaCuts.addCut("fGoodTracksPt", "p_{T,trk} [GeV]", fGoodTracksPt);
-  fAnaCuts.addCut("fGoodTracksEta", "#eta_{trk} ", fGoodTracksEta);
-
-  fAnaCuts.addCut("fGoodQ", "q_{1} 1_{2}", fGoodQ);
-  fAnaCuts.addCut("fGoodPvAveW8", "<w8>", fGoodPvAveW8);
-  fAnaCuts.addCut("fGoodIp", "IP", fGoodIp);
-  fAnaCuts.addCut("fGoodIpS", "IPS", fGoodIpS);
-  fAnaCuts.addCut("fGoodLip", "LIP", fGoodLip);
-  fAnaCuts.addCut("fGoodLipS", "LIPS", fGoodLipS);
-  fAnaCuts.addCut("fGoodMaxDoca", "MAXDOCA", fGoodMaxDoca);
-  fAnaCuts.addCut("fGoodPt", "p_{T,B}", fGoodPt);
-  fAnaCuts.addCut("fGoodEta", "#eta_{B}", fGoodEta);
-  fAnaCuts.addCut("fGoodAlpha", "#alpha", fGoodAlpha);
-  fAnaCuts.addCut("fGoodFLS", "l/#sigma(l)", fGoodFLS);
-  fAnaCuts.addCut("fGoodChi2", "#chi^{2}", fGoodChi2);
-  fAnaCuts.addCut("fGoodIso", "I_{trk}", fGoodIso);
-  fAnaCuts.addCut("fGoodCloseTrack", "close track veto", fGoodCloseTrack);
-  fAnaCuts.addCut("fGoodDocaTrk", "d_{ca}(trk)", fGoodDocaTrk);
-  fAnaCuts.addCut("fGoodBDT", "bdt", fGoodBDT);
-  fAnaCuts.addCut("fGoodLastCut", "lastCut", fGoodLastCut);
-
+  printCuts(cout);
 
   fChan = 0;
 
@@ -82,6 +46,15 @@ plotWork::plotWork(string dir, string files, string cuts, string setup): plotCla
 // ----------------------------------------------------------------------
 plotWork::~plotWork() {
 
+}
+
+
+
+// ----------------------------------------------------------------------
+void plotWork::init() {
+  system(Form("/bin/rm -f %s", fHistFileName.c_str()));
+  system(Form("/bin/rm -f %s", fNumbersFileName.c_str()));
+  system(Form("/bin/rm -f %s", fTexFileName.c_str()));
 }
 
 
@@ -95,194 +68,81 @@ void plotWork::makeAll(int bitmask) {
 
 
 // ----------------------------------------------------------------------
-void plotWork::privateVsOfficial(string mode, int nevt) {
-  MASSMIN = 4.5;
-  MASSMAX = 6.5;
-  string dir("candAnaMuMu");
-  if (string::npos != mode.find("bdmm")) {
-    fSetup = "SgMc";
-    fIsSignal = true;
-    BGLBOXMIN = 4.80;
-    BGLBOXMAX = 5.20;
-    SIGBOXMIN = 5.20;
-    SIGBOXMAX = 5.45;
-    BGHBOXMIN = 5.45;
-    BGHBOXMAX = 6.00;
-  }
+void plotWork::bookHist(string dsname) {
 
-  if (string::npos != mode.find("bujpsikp")) {
-    BGLBOXMIN = 5.00;
-    BGLBOXMAX = 5.18;
-    SIGBOXMIN = 5.23;
-    SIGBOXMAX = 5.33;
-    BGHBOXMIN = 5.40;
-    BGHBOXMAX = 5.50;
-  }
+  fpHnorm = new TH1D(Form("h_%s_%s", "norm", dsname.c_str()), Form("h_%s_%s", "all", dsname.c_str()), 40, 4.8, 6.0);
+  fpHpass = new TH1D(Form("h_%s_%s", "pass", dsname.c_str()), Form("h_%s_%s", "all", dsname.c_str()), 40, 4.8, 6.0);
 
-  if (string::npos != mode.find("bsjpsiphi")) {
-    BGLBOXMIN = 5.10;
-    BGLBOXMAX = 5.29;
-    SIGBOXMIN = 5.34;
-    SIGBOXMAX = 5.40;
-    BGHBOXMIN = 5.45;
-    BGHBOXMAX = 5.70;
-  }
-
-  TTree *t(0);
-  string lmode = mode;
-  fDS[lmode]->fName = "private";
-  t = getTree(lmode, dir);
-  setupTree(t, fSetup);
-  fOffset = 0;
-  bookDistributions(lmode);
-  int nevents(-1);
-  if (nevt > -1) nevents = nevt;
-  loopOverTree(t, 1, nevents);
-
-  lmode = mode + "_official";
-  fDS[lmode]->fName = "official";
-  fDS[lmode]->fColor = kRed+2;
-  fDS[lmode]->fLcolor = kRed+2;
-  fDS[lmode]->fFcolor = kRed+2;
-  fDS[lmode]->fFillStyle = 3356;
-  t = getTree(lmode, dir);
-  setupTree(t, fSetup);
-  fOffset = 1;
-  bookDistributions(lmode);
-  loopOverTree(t, 1, nevents);
-
-  // a0->hPresel[2]->Draw("e");
-  // a1->hPresel[2]->Scale(a0->hPresel[2]->GetSumOfWeights()/a1->hPresel[2]->GetSumOfWeights());
-  // a1->hPresel[2]->Draw("samehist");
-
-  //  gStyle->SetOptTitle(0);
-
-
-  TH1* h0 = fpMuon1Pt[0]->hPresel[2];
-  TH1* h1 = fpMuon1Pt[1]->hPresel[2];  h0->SetTitle("");
-  fDS[mode]->setHistStyle(h0);  fDS[lmode]->setHistStyle(h1);   overlay(h0, mode, h1, lmode, 0, "", UNITY, false, true, 0.4, 0.7);
-  savePad(Form("mcval-m1pt-%s-%s.pdf", mode.c_str(), lmode.c_str()));
-
-  h0 = fpMuon2Pt[0]->hPresel[2];  h1 = fpMuon2Pt[1]->hPresel[2];  h0->SetTitle("");
-  fDS[mode]->setHistStyle(h0);  fDS[lmode]->setHistStyle(h1);   overlay(h0, mode, h1, lmode, 0, "", UNITY, false, true, 0.4, 0.7);
-  savePad(Form("mcval-m2pt-%s-%s.pdf", mode.c_str(), lmode.c_str()));
-
-  h0 = fpPt[0]->hPresel[2];  h1 = fpPt[1]->hPresel[2]; h0->SetTitle("");
-  fDS[mode]->setHistStyle(h0);  fDS[lmode]->setHistStyle(h1);   overlay(h0, mode, h1, lmode, 0, "", UNITY, false, true, 0.4, 0.7);
-  savePad(Form("mcval-pt-%s-%s.pdf", mode.c_str(), lmode.c_str()));
-
-  h0 = fpIso[0]->hPresel[2];  h1 = fpIso[1]->hPresel[2]; h0->SetTitle("");
-  fDS[mode]->setHistStyle(h0);  fDS[lmode]->setHistStyle(h1);   overlay(h0, mode, h1, lmode, 0, "", UNITY, false, true, 0.4, 0.7);
-  savePad(Form("mcval-iso-%s-%s.pdf", mode.c_str(), lmode.c_str()));
-
-  h0 = fpFLS3d[0]->hPresel[2];  h1 = fpFLS3d[1]->hPresel[2]; h0->SetTitle("");
-  fDS[mode]->setHistStyle(h0);  fDS[lmode]->setHistStyle(h1);   overlay(h0, mode, h1, lmode, 0, "", UNITY, false, true, 0.4, 0.7);
-  savePad(Form("mcval-fls3d-%s-%s.pdf", mode.c_str(), lmode.c_str()));
-
-  h0 = fpIpS[0]->hPresel[2];  h1 = fpIpS[1]->hPresel[2]; h0->SetTitle("");
-  fDS[mode]->setHistStyle(h0);  fDS[lmode]->setHistStyle(h1);   overlay(h0, mode, h1, lmode, 0, "", UNITY, false, true, 0.4, 0.7);
-  savePad(Form("mcval-ips-%s-%s.pdf", mode.c_str(), lmode.c_str()));
-
-  h0 = fpIp[0]->hPresel[2];  h1 = fpIp[1]->hPresel[2];  h0->SetTitle("");
-  fDS[mode]->setHistStyle(h0);  fDS[lmode]->setHistStyle(h1);   overlay(h0, mode, h1, lmode, 0, "", UNITY, false, true, 0.4, 0.7);
-  savePad(Form("mcval-ip-%s-%s.pdf", mode.c_str(), lmode.c_str()));
-
-  h0 = fpPvN[0]->hPresel[2];  h1 = fpPvN[1]->hPresel[2]; h0->SetTitle("");
-  fDS[mode]->setHistStyle(h0);  fDS[lmode]->setHistStyle(h1);   overlay(h0, mode, h1, lmode, 0, "", UNITY, false, true, 0.4, 0.7);
-  savePad(Form("mcval-pvn-%s-%s.pdf", mode.c_str(), lmode.c_str()));
 }
 
 
+
+
 // ----------------------------------------------------------------------
-void plotWork::loopFunction1() {
+void plotWork::tisEfficiency(string dsname) {
 
-  double mass = fb.cm;
-  if (fIsMC) mass = fb.m;
-  if (fIsSignal) mass = fb.m;
+  // -- dump histograms
+  cout << "fHistFile: " << fHistFileName;
+  fHistFile = TFile::Open(fHistFileName.c_str(), "UPDATE");
+  cout << " opened " << endl;
 
-  TLorentzVector a;
-  a.SetPtEtaPhiM(fb.pt,fb.eta,fb.phi,mass);
+  fSample = dsname;
+  string dir = "candAnaBu2JpsiK";
 
-  fpMuon1Pt[fOffset]->fill(fb.m1pt, mass);
-  fpMuon2Pt[fOffset]->fill(fb.m2pt, mass);
+  TTree *t = getTree(fSample, dir);
+  if (0 == t) {
+    cout << "tree for sample = " << fSample << " not found" << endl;
+    return;
+  }
+  bookHist(fSample);
+  setupTree(t, fSample);
+  fCds = fSample;
+  loopOverTree(t, 1);
 
-  fpMuonsEta[fOffset]->fill(fb.m1eta, mass);
-  fpMuonsEta[fOffset]->fill(fb.m2eta, mass);
-  fpPt[fOffset]->fill(fb.pt, mass);
-  fpP[fOffset]->fill(a.P(), mass);
-  fpPz[fOffset]->fill(a.Pz(), mass);
-  fpEta[fOffset]->fill(fb.eta, mass);
-  fpAlpha[fOffset]->fill(fb.alpha, mass);
 
-  fpIso[fOffset]->fill(fb.iso, mass);
-  fpCloseTrk[fOffset]->fill(fb.closetrk, mass);
-  fpDocaTrk[fOffset]->fill(fb.docatrk, mass);
-
-  fpChi2Dof[fOffset]->fill(fb.chi2/fb.dof, mass);
-  fpPChi2Dof[fOffset]->fill(fb.pchi2dof, mass);
-
-  fpFLS3d[fOffset]->fill(fb.fls3d, mass);
-  fpFL3d[fOffset]->fill(fb.fl3d, mass);
-  fpFL3dE[fOffset]->fill(fb.fl3dE, mass);
-
-  fpMaxDoca[fOffset]->fill(fb.maxdoca, mass);
-  fpIp[fOffset]->fill(fb.pvip, mass);
-  fpIpS[fOffset]->fill(fb.pvips, mass);
-  fpPvZ[fOffset]->fill(fb.pvz, mass);
-  fpPvN[fOffset]->fill(fb.pvn, mass);
-  fpPvAveW8[fOffset]->fill(fb.pvw8, mass);
-
+  fpHnorm->Write();
+  fpHpass->Write();
+  fHistFile->Close();
 
 }
 
 
 
 // ----------------------------------------------------------------------
-void plotWork::bookDistributions(string mode) {
-  string name = Form("%s_%s_", fSetup.c_str(), mode.c_str());
-  fpMuon1Pt[fOffset]   = bookDistribution(Form("%smuon1pt", name.c_str()), "p_{T, #mu1} [GeV]", "fGoodMuonsPt", 60, 0., 30.);
-  fpMuon2Pt[fOffset]   = bookDistribution(Form("%smuon2pt", name.c_str()), "p_{T, #mu2} [GeV]", "fGoodMuonsPt", 40, 0., 20.);
-  fpMuonsEta[fOffset]  = bookDistribution(Form("%smuonseta", name.c_str()), "#eta_{#mu}", "fGoodMuonsEta", 40, -2.5, 2.5);
-  fpPt[fOffset]        = bookDistribution(Form("%spt", name.c_str()), "p_{T}(B) [GeV]", "fGoodPt", 60, 0., 60.);
-  fpP[fOffset]         = bookDistribution(Form("%sp", name.c_str()), "p(B) [GeV]", "fGoodPt", 50, 0., 100.);
-  fpPz[fOffset]        = bookDistribution(Form("%spz", name.c_str()), "p_{z}(B) [GeV]", "fGoodPt", 50, 0., 100.);
-  fpEta[fOffset]       = bookDistribution(Form("%seta", name.c_str()), "#eta(B)", "fGoodEta", 40, -2.5, 2.5);
-  fpAlpha[fOffset]     = bookDistribution(Form("%salpha", name.c_str()), "#alpha_{3D}", "fGoodAlpha", 50, 0., 0.1);
-  fpIso[fOffset]       = bookDistribution(Form("%siso", name.c_str()),  "isolation", "fGoodIso", 52, 0., 1.04);
-  fpCloseTrk[fOffset]  = bookDistribution(Form("%sclosetrk", name.c_str()),  "N_{trk}^{close}", "fGoodCloseTrack", 10, 0., 10.);
-  fpDocaTrk[fOffset]   = bookDistribution(Form("%sdocatrk", name.c_str()), "d_{ca}^{0} [cm]", "fGoodDocaTrk", 50, 0., 0.20);
+void plotWork::loopFunction1() {
 
-  fpChi2Dof[fOffset]   = bookDistribution(Form("%schi2dof", name.c_str()),  "#chi^{2}/dof", "fGoodChi2", 40, 0., 4.);
-  fpPChi2Dof[fOffset]  = bookDistribution(Form("%spchi2dof", name.c_str()),  "P(#chi^{2},dof)", "fGoodChi2", 50, 0., 1.0);
+  if (!fb.tis) return;
 
-  fpFLS3d[fOffset]     = bookDistribution(Form("%sfls3d", name.c_str()), "l_{3D}/#sigma(l_{3D})", "fGoodFLS", 60, 0., 120.);
-  fpFL3d[fOffset]      = bookDistribution(Form("%sfl3d", name.c_str()),  "l_{3D} [cm]", "fGoodFLS", 60, 0., 1.5);
-  fpFL3dE[fOffset]     = bookDistribution(Form("%sfl3de", name.c_str()), "#sigma(l_{3D}) [cm]", "fGoodFLS", 50, 0., 0.05);
+  if (!fGoodQ) return;
+  if (!fGoodPvAveW8) return;
+  if (!fGoodMaxDoca) return;
+  if (!fGoodIp) return;
+  if (!fGoodIpS) return;
 
-  fpMaxDoca[fOffset]   = bookDistribution(Form("%smaxdoca", name.c_str()), "d^{max} [cm]", "fGoodMaxDoca", 60, 0., 0.03);
-  fpIp[fOffset]        = bookDistribution(Form("%sip", name.c_str()), "#delta_{3D} [cm]", "fGoodIp", 50, 0., 0.020);
-  fpIpS[fOffset]       = bookDistribution(Form("%sips", name.c_str()), "#delta_{3D}/#sigma(#delta_{3D})", "fGoodIpS", 50, 0., 4);
+  if (!fGoodLip) return;
+  if (!fGoodLipS) return;
 
-  fpPvZ[fOffset]       = bookDistribution(Form("%spvz", name.c_str()), "z_{PV} [cm]", "fGoodHLT", 40, -20., 20.);
-  fpPvN[fOffset]       = bookDistribution(Form("%spvn", name.c_str()), "N(PV) ", "fGoodHLT", 40, 0., 40.);
-  fpPvAveW8[fOffset]   = bookDistribution(Form("%spvavew8", name.c_str()), "<w^{PV}>", "fGoodPvAveW8", 50, 0.5, 1.);
+  if (!fGoodAlpha) return;
+  if (!fGoodChi2) return;
+  if (!fGoodFLS) return;
 
+  if (!fGoodCloseTrack) return;
+  if (!fGoodIso) return;
+  if (!fGoodDocaTrk) return;
+
+  if (TMath::Abs(fb.flsxy) < 3.0) return;
+
+  if (TMath::Abs(fb.m1eta) > 1.6) return;
+  if (TMath::Abs(fb.m2eta) > 1.6) return;
+
+
+  fpHnorm->Fill(fb.m);
+
+  if (!fb.hlt) return;
+  fpHpass->Fill(fb.m);
 
 }
 
-
-
-// ----------------------------------------------------------------------
-AnalysisDistribution* plotWork::bookDistribution(string hn, string ht, string hc, int nbins, double lo, double hi) {
-  AnalysisDistribution *p = new AnalysisDistribution(hn.c_str(), ht.c_str(), nbins, lo, hi);
-  p->setSigWindow(SIGBOXMIN, SIGBOXMAX);
-  p->setBg1Window(BGLBOXMIN, BGLBOXMAX);
-  p->setBg2Window(BGHBOXMIN, BGHBOXMAX);
-  p->setAnalysisCuts(&fAnaCuts, hc.c_str());
-  p->setPreselCut(&fPreselection);
-
-  return p;
-}
 
 
 // ----------------------------------------------------------------------
@@ -542,34 +402,12 @@ void plotWork::prodSummary(string ds1, int year) {
 
 
 // ----------------------------------------------------------------------
-void plotWork::setCuts(string cuts) {
-  cout << "==> plotWork::setCuts: " << cuts << endl;
-
-  istringstream ss(cuts);
-  string token, name, sval;
-
-  while (getline(ss, token, ',')) {
-
-    string::size_type m1 = token.find("=");
-    name = token.substr(0, m1);
-    sval = token.substr(m1+1);
-
-    if (string::npos != name.find("PTLO")) {
-      float val;
-      val = atof(sval.c_str());
-      PTLO = val;
-    }
-
-  }
-}
-
-
-// ----------------------------------------------------------------------
 void plotWork::loadFiles(string afiles) {
 
   string files = fDirectory + string("/") + afiles;
-  cout << "==> Loading files listed in " << files << endl;
+  cout << "==> plotWork::loadFile loading files listed in " << files << endl;
 
+  bool readMoreFiles(false);
   char buffer[1000];
   ifstream is(files.c_str());
   while (is.getline(buffer, 1000, '\n')) {
@@ -587,94 +425,68 @@ void plotWork::loadFiles(string afiles) {
     string::size_type m2 = sbuffer.find("file=");
     string slumi = sbuffer.substr(m1+5, m2-m1-6);
     string sfile = sbuffer.substr(m2+5);
-    string sname, sdecay;
-
-    //    cout << "stype: ->" << stype << "<-" << endl;
+    string sname("nada"), sdecay("nada");
 
     TFile *pF(0);
+    dataset *ds(0);
+
     if (string::npos != stype.find("data")) {
       // -- DATA
       pF = loadFile(sfile);
 
-      dataset *ds = new dataset();
-      ds->fSize = 1;
+      ds = new dataset();
+      ds->fSize = 1.2;
       ds->fWidth = 2;
-
-      if (string::npos != stype.find("bmm")) {
-        sname = "data_bmm";
-        sdecay = "bmm";
+      if (string::npos != stype.find("XXX")) {
+        sname = "bdpsikstarData";
+        sdecay = "bdpsikstar";
 	ds->fColor = kBlack;
-	ds->fSymbol = 24;
+	ds->fSymbol = 20;
 	ds->fF      = pF;
 	ds->fBf     = 1.;
 	ds->fMass   = 1.;
 	ds->fFillStyle = 3365;
+	fDS.insert(make_pair(sname, ds));
       }
-
-      if (string::npos != stype.find("bu2jpsik")) {
-        sname = "data_bu2jpsik";
-        sdecay = "bu2jpsik";
-	ds->fColor = kBlack;
-	ds->fSymbol = 24;
-	ds->fF      = pF;
-	ds->fBf     = 1.;
-	ds->fMass   = 1.;
-	ds->fFillStyle = 3365;
-      }
-
-      ds->fLcolor = ds->fColor;
-      ds->fFcolor = ds->fColor;
-      ds->fName   = sdecay;
-      ds->fFullName = sname;
-      fDS.insert(make_pair(sname, ds));
-
 
     } else {
       // -- MC
       pF = loadFile(sfile);
-      //      cout << "  " << sfile << ": " << pF << endl;
 
-      dataset *ds = new dataset();
-      ds->fSize = 1;
-      ds->fWidth = 2;
+      ds = new dataset();
+      ds->fSize = 0.1;
+      ds->fWidth = 2.;
 
-      string filter = "";
-      if (string::npos != stype.find("acc")) filter = "_acc";
-
-      string prod = "";
-      if (string::npos != stype.find("official")) filter = "_official";
-
-      // -------------------------
-      // -- genAnalysis files below
-      // -------------------------
-      if (string::npos != stype.find("bdmm,")) {
-        sname = "bdmm" + filter + prod;
-        sdecay = "bdmm";
-	ds->fColor = kBlue-7;
+      if (string::npos != stype.find("YYY")) {
+        sname = "bupsikMc";
+        sdecay = "bupsik";
+	ds->fColor = kGreen-2;
 	ds->fSymbol = 24;
+	ds->fWidth  = 2.;
 	ds->fF      = pF;
 	ds->fBf     = 1.;
 	ds->fMass   = 1.;
-	ds->fFillStyle = 3365;
+	ds->fFillStyle = 3354;
+	fDS.insert(make_pair(sname, ds));
       }
 
-      //      cout << "  inserting as name ->" << sname << "<- and decay = " << sdecay << endl;
+    }
+    if (sname != "nada") {
+      readMoreFiles = true;
       ds->fLcolor = ds->fColor;
       ds->fFcolor = ds->fColor;
       ds->fName   = sdecay;
       ds->fFullName = sname;
       fDS.insert(make_pair(sname, ds));
-
-
-
     }
 
 
   }
 
   is.close();
-  cout << "Summary of files loaded: " << endl;
-  for (map<string, dataset*>::iterator it = fDS.begin(); it != fDS.end(); ++it) {
-    cout << "    " << it->first << ": " << it->second->fName << ", " << it->second->fF->GetName() << endl;
+  if (readMoreFiles) {
+    for (map<string, dataset*>::iterator it = fDS.begin(); it != fDS.end(); ++it) {
+      cout << it->first << ": " << it->second->fName << ", " << it->second->fF->GetName() << endl;
+    }
   }
 }
