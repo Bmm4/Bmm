0ce56b7a60cfa9b46fef72e3c7267566e616ffbf
Urs Langenegger <urs.langenegger@psi.ch>
2016-06-23 16:20:43 +0200

    added Lumi class to keep track of run:lumi

---
+++ RootAnalysis/common/Lumi.hh
@@ -0,0 +1,43 @@
+#ifndef LUMI_HH
+#define LUMI_HH
+
+#include <utility>
+#include <vector>
+#include <string>
+#include <map>
+
+// ----------------------------------------------------------------------
+// -- Lumi
+// -------
+//
+// class to parse the output of bril_calc and provide a map run:lumi
+//
+// Usage:
+//          Lumi a("/shome/ursl/json/1.lumi");
+//          double lumi = a.lumi(run);
+//
+// ----------------------------------------------------------------------
+
+class Lumi {
+public:
+  Lumi(std::string fname, int verbose = 1);
+  ~Lumi();
+
+  void parse(std::string fname);
+
+  void print();
+  double lumi(int run);
+
+private:
+
+  std::vector<std::string>& split(const std::string &s, char delim, std::vector<std::string> &elems);
+  std::vector<std::string> split(const std::string &s, char delim);
+
+
+  std::map<int, double> fMapRunLumi;
+  std::string           fNormtag;
+  int fVerbose;
+};
+
+
+#endif
