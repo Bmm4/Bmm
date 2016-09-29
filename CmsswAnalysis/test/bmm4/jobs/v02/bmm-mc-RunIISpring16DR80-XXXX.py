1b47ef7172429573ed77b4f9d7dc885217e9e67f
Urs Langenegger <urs.langenegger@psi.ch>
2016-09-29 10:50:14 +0200

    change GT

--- CmsswAnalysis/test/bmm4/jobs/v02/bmm-mc-RunIISpring16DR80-XXXX.py
+++ CmsswAnalysis/test/bmm4/jobs/v02/bmm-mc-RunIISpring16DR80-XXXX.py
@@ -20,7 +20,7 @@ process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 
-process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_v3', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_RealisticBS_25ns_13TeV2016_v1_mc', '')
 
 # ----------------------------------------------------------------------
 # POOLSOURCE
