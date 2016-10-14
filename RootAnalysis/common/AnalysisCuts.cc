eace0a1959e3d828e89e09fc9270bde1046cbc28
Urs Langenegger <urs.langenegger@psi.ch>
2016-10-14 16:06:56 +0200

    add clear()

--- RootAnalysis/common/AnalysisCuts.cc
+++ RootAnalysis/common/AnalysisCuts.cc
@@ -9,15 +9,15 @@ ClassImp(AnalysisCuts)
 // ----------------------------------------------------------------------
 AnalysisCuts::AnalysisCuts() {
   for (int i = 0; i < MAXCUTS; ++i) {
-    fCutName[i]     = TString("unused"); 
-    fDescription[i] = TString("nothing said"); 
+    fCutName[i]     = TString("unused");
+    fDescription[i] = TString("nothing said");
     fCutValue[i]    = false;
     fAocValue[i]    = false;
     fCumValue[i]    = false;
     fNm1Value[i]    = false;
  }
-  fNcuts = 0; 
-  fUpdated = 0; 
+  fNcuts   = 0;
+  fUpdated = 0;
 }
 
 
@@ -30,19 +30,19 @@ AnalysisCuts::~AnalysisCuts() {
 
 // // ----------------------------------------------------------------------
 // void AnalysisCuts::addCut(const char *name, int &location) {
-//   addCut(name, "no description", location); 
+//   addCut(name, "no description", location);
 // }
 
 
 // // ----------------------------------------------------------------------
 // void AnalysisCuts::addCut(const char *name, const char *description, int &location) {
-//   fCutName[fNcuts]     = TString(name); 
-//   fDescription[fNcuts] = TString(description); 
-//   fCutLocation[fNcuts] = &location; 
-//   fAocValue[fNcuts]    = -1; 
-//   fCumValue[fNcuts]    = -1; 
+//   fCutName[fNcuts]     = TString(name);
+//   fDescription[fNcuts] = TString(description);
+//   fCutLocation[fNcuts] = &location;
+//   fAocValue[fNcuts]    = -1;
+//   fCumValue[fNcuts]    = -1;
 //   cout << "--> Added cut " << name << "  " << description << endl;
-//   fNcuts++; 
+//   fNcuts++;
 // }
 
 
@@ -50,95 +50,111 @@ AnalysisCuts::~AnalysisCuts() {
 // ----------------------------------------------------------------------
 void AnalysisCuts::dumpAll() {
   for (int i = 0; i < fNcuts; ++i) {
-    cout << Form("Cut %2d: %s (%s) at %d", i, fCutName[i].Data(), fDescription[i].Data(), (fCutLocation[i]?1:0)) 
+    cout << Form("Cut %2d: %s (%s) at %d", i, fCutName[i].Data(), fDescription[i].Data(), (fCutLocation[i]?1:0))
 	 << Form(" nm=%i si=%i cu=%i", (fNm1Value[i]?1:0),  (fCutValue[i]?1:0),  (fCumValue[i]?1:0))
-	 << endl; 
+	 << endl;
   }
 }
 
 // ----------------------------------------------------------------------
+void AnalysisCuts::clear() {
+  for (int i = 0; i < fNcuts; ++i) {
+    fCutName[i]     = "cleared";
+    fDescription[i] = "cleared";
+    fCutValue[i]    = false;
+    fAocValue[i]    = false;
+    fCumValue[i]    = false;
+    fNm1Value[i]    = false;
+    fCutLocation[i] = 0;
+  }
+
+  fNcuts = 0;
+  fUpdated =0;
+}
+
+// ----------------------------------------------------------------------
 void AnalysisCuts::addCut(const char *name, bool &location) {
-  addCut(name, "no description", location); 
+  addCut(name, "no description", location);
 }
 
 
 // ----------------------------------------------------------------------
 void AnalysisCuts::addCut(const char *name, const char *description, bool &location) {
-  fCutName[fNcuts]     = TString(name); 
-  fDescription[fNcuts] = TString(description); 
-  fCutLocation[fNcuts] = &location; 
-  fAocValue[fNcuts]    = false; 
-  fCumValue[fNcuts]    = false; 
+  fCutName[fNcuts]     = TString(name);
+  fDescription[fNcuts] = TString(description);
+  fCutLocation[fNcuts] = &location;
+  fAocValue[fNcuts]    = false;
+  fCumValue[fNcuts]    = false;
   fNm1Value[fNcuts]    = false;
   cout << "--> Added cut " << name << "  " << description << endl;
-  fNcuts++; 
+  fNcuts++;
 }
 
 
 // ----------------------------------------------------------------------
 int AnalysisCuts::getIndex(const char *name) {
   for (int i = 0; i < fNcuts; ++i) {
-    if (!strcmp(fCutName[i].Data(), name)) return i; 
+    if (!strcmp(fCutName[i].Data(), name)) return i;
   }
-  return -1; 
+  return -1;
 }
 
 // ----------------------------------------------------------------------
 const char* AnalysisCuts::getName(int icut) {
   if ((icut >= 0) && (icut < fNcuts)) {
-    return fCutName[icut].Data(); 
+    return fCutName[icut].Data();
   } else {
-    return "xx> AnalysisCuts: cut index out of range"; 
+    return "xx> AnalysisCuts: cut index out of range";
   }
 }
 
 // ----------------------------------------------------------------------
 const char* AnalysisCuts::getDescription(int icut) {
   if ((icut >= 0) && (icut < fNcuts)) {
-    return fDescription[icut].Data(); 
+    return fDescription[icut].Data();
   } else {
-    return "xx> AnalysisCuts: cut index out of range"; 
+    return "xx> AnalysisCuts: cut index out of range";
   }
 }
 
 // ----------------------------------------------------------------------
 void AnalysisCuts::update() {
-  // -- Fill all cuts 
+  // -- Fill all cuts
   for (int i = 0; i < fNcuts; ++i) {
-    fCutValue[i] = *(fCutLocation[i]); 
+    fCutValue[i] = *(fCutLocation[i]);
   }
 
-  // -- Cache derived information 
+  // -- Cache derived information
   for (int i = 0; i < fNcuts; ++i) {
-    fNm1Value[i] = true; 
+    fNm1Value[i] = true;
     for (int j = 0; j < i; ++j) {
       if (singleCutTrue(j) == false) {
-	fNm1Value[i] = false; 
+	fNm1Value[i] = false;
 	break;
       }
     }
 
 
-    fCumValue[i] = true; 
+    fCumValue[i] = true;
     for (int j = 0; j <= i; ++j) {
       if (singleCutTrue(j) == false) {
-	fCumValue[i] = false; 
+	fCumValue[i] = false;
 	break;
       }
     }
 
-    fAocValue[i] = true; 
+    fAocValue[i] = true;
     for (int j = 0; j < fNcuts; ++j) {
-      if (j == i) continue; 
+      if (j == i) continue;
       if (singleCutTrue(j) == false) {
-	fAocValue[i] = false; 
+	fAocValue[i] = false;
 	break;
       }
     }
-  }    
+  }
 
   if (0 == fUpdated)  cout << "Updating AnalysisCuts for the first time" << endl;
-  fUpdated = 1; 
+  fUpdated = 1;
 
 }
 
@@ -171,7 +187,7 @@ bool AnalysisCuts::singleCutTrue(const char *name) {
   } else {
     cout << "xx> AnalysisCuts: Unknown cut " << name << endl;
     return false;
-  }    
+  }
 }
 
 // ----------------------------------------------------------------------
@@ -182,7 +198,7 @@ bool AnalysisCuts::cumulativeCutTrue(const char *name) {
   } else {
     cout << "xx> AnalysisCuts: Unknown cut " << name << endl;
     return false;
-  }    
+  }
 }
 
 // ----------------------------------------------------------------------
@@ -193,7 +209,7 @@ bool AnalysisCuts::nMinus1CutsTrue(const char *name) {
   } else {
     cout << "xx> AnalysisCuts: Unknown cut " << name << endl;
     return false;
-  }    
+  }
 }
 
 // ----------------------------------------------------------------------
@@ -204,5 +220,5 @@ bool AnalysisCuts::allOtherCutsTrue(const char *name) {
   } else {
     cout << "xx> AnalysisCuts: Unknown cut " << name << endl;
     return false;
-  }    
+  }
 }
--- RootAnalysis/common/AnalysisCuts.hh
+++ RootAnalysis/common/AnalysisCuts.hh
@@ -15,18 +15,18 @@ using namespace::std;
 // ----------------------------------------------------------------------
 // AnalysisCuts
 // ------------
-// Class to organize cut sequences in an analysis. It allows to 
-// ask the object whether a given cut is fulfilled, for cumulative cut 
-// fulfilment (in the order of definition), and whether 
-// "all other cuts" are fulfilled. 
+// Class to organize cut sequences in an analysis. It allows to
+// ask the object whether a given cut is fulfilled, for cumulative cut
+// fulfilment (in the order of definition), and whether
+// "all other cuts" are fulfilled.
 //
-// You can access cuts by index (good for loops) or name (good for 
-// less confusion). 
+// You can access cuts by index (good for loops) or name (good for
+// less confusion).
 //
 // Don't forget to call update() before using it (i.e. after the cuts
 // have been computed in your analysis)
 //
-// Bugs: Should probably be inlined to speed. But this seems not to work 
+// Bugs: Should probably be inlined to speed. But this seems not to work
 //       in CINT (???)
 // ----------------------------------------------------------------------
 
@@ -35,50 +35,51 @@ class AnalysisCuts: public TObject {
 
 public:
 
-  AnalysisCuts(); 
-  ~AnalysisCuts(); 
-  void dumpAll(); 
+  AnalysisCuts();
+  ~AnalysisCuts();
+  void dumpAll();
 
-  int getIndex(const char *name); 
-  const char* getName(int i); 
-  const char* getDescription(int i); 
+  int getIndex(const char *name);
+  const char* getName(int i);
+  const char* getDescription(int i);
 
-  //   void addCut(const char *name, int &location); 
-  //   void addCut(const char *name, const char *description, int &location); 
+  //   void addCut(const char *name, int &location);
+  //   void addCut(const char *name, const char *description, int &location);
 
-  void addCut(const char *name, bool &location); 
-  void addCut(const char *name, const char *description, bool &location); 
+  void addCut(const char *name, bool &location);
+  void addCut(const char *name, const char *description, bool &location);
 
-  bool singleCutTrue(int i); 
-  bool singleCutTrue(const char *name); 
+  bool singleCutTrue(int i);
+  bool singleCutTrue(const char *name);
 
-  bool cumulativeCutTrue(int i); 
-  bool cumulativeCutTrue(const char *name); 
+  bool cumulativeCutTrue(int i);
+  bool cumulativeCutTrue(const char *name);
 
-  bool nMinus1CutsTrue(int i); 
-  bool nMinus1CutsTrue(const char *name); 
-  
-  bool allOtherCutsTrue(int i); 
-  bool allOtherCutsTrue(const char *name); 
+  bool nMinus1CutsTrue(int i);
+  bool nMinus1CutsTrue(const char *name);
 
-  void update(); 
+  bool allOtherCutsTrue(int i);
+  bool allOtherCutsTrue(const char *name);
+
+  void update();
+  void clear();
   int  ncuts() {return fNcuts;}
 
-private: 
-  
-  int fNcuts; 
-  int fUpdated; 
-  TString fCutName[MAXCUTS]; 
-  TString fDescription[MAXCUTS]; 
-  bool    fCutValue[MAXCUTS]; 
-  bool    fAocValue[MAXCUTS]; 
-  bool    fCumValue[MAXCUTS]; 
-  bool    fNm1Value[MAXCUTS]; 
-  
-  bool    *fCutLocation[MAXCUTS]; 
+private:
+
+  int fNcuts;
+  int fUpdated;
+  TString fCutName[MAXCUTS];
+  TString fDescription[MAXCUTS];
+  bool    fCutValue[MAXCUTS];
+  bool    fAocValue[MAXCUTS];
+  bool    fCumValue[MAXCUTS];
+  bool    fNm1Value[MAXCUTS];
+
+  bool    *fCutLocation[MAXCUTS];
 
   ClassDef(AnalysisCuts,1) //Testing AnalysisCuts
 
-}; 
+};
 
 #endif
