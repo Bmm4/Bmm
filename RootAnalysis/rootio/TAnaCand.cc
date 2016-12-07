1041ba349da468808cea0c5720bc1b58196cea9a
Urs Langenegger <urs.langenegger@psi.ch>
2016-12-07 09:54:36 +0100

    add constrained mass printout

--- RootAnalysis/rootio/TAnaCand.cc
+++ RootAnalysis/rootio/TAnaCand.cc
@@ -42,8 +42,8 @@ void TAnaCand::clear() {
 
 
 void TAnaCand::dump() {
-  cout << Form("Cand: idx=%3d type=%d m=%5.3f, pT=%6.2f f=%+4.3f eta=%+4.3f maxdoca=%4.3f pvips=%4.3f",
-	       fIndex, fType, fMass, fPlab.Perp(), fPlab.Phi(), fPlab.Eta(), fMaxDoca, (fPvIP3dE>0.?fPvIP3d/fPvIP3dE:-1.))
+  cout << Form("Cand: idx=%3d type=%d m=%5.3f cm=%5.3f, pT=%6.2f f=%+4.3f eta=%+4.3f maxdoca=%4.3f pvips=%4.3f",
+	       fIndex, fType, fMass, fMassC, fPlab.Perp(), fPlab.Phi(), fPlab.Eta(), fMaxDoca, (fPvIP3dE>0.?fPvIP3d/fPvIP3dE:-1.))
        << endl;
 
   cout << "mother cand: " << fMom << " daughter cands: " << fDau1 << " .. " << fDau2  << " sig tracks: " << fSig1 << " .. " << fSig2 << endl;
