15a53cc0edbfda5f30272e27468af2e664fb8070
Urs Langenegger <urs.langenegger@psi.ch>
2017-01-03 15:14:54 +0100

    updated onia selection

--- CmsswAnalysis/python/HFOnia_cff.py
+++ CmsswAnalysis/python/HFOnia_cff.py
@@ -1,20 +1,26 @@
 import FWCore.ParameterSet.Config as cms
 
+
 # ----------------------------------------------------------------------
 psiDump = cms.EDAnalyzer(
     "HFDiTracks",
     verbose            = cms.untracked.int32(0),
     muonsLabel         = cms.untracked.InputTag("muons"),
-    tracksLabel        = cms.untracked.InputTag('generalTracks'),
+    tracksLabel        = cms.untracked.InputTag("generalTracks"),
     PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
     BeamSpotLabel      = cms.untracked.InputTag("offlineBeamSpot"),
-    unlikeCharge       = cms.untracked.bool(True),
+    muonQualityString  = cms.untracked.string("AllGlobalMuons"),
     muonPt             = cms.untracked.double(4.0),
-    chi2               = cms.untracked.double(5.),
+    pt                 = cms.untracked.double(5.),
+    chi2               = cms.untracked.double(10.),
+    pvips              = cms.untracked.double(5.),
+    flsxy              = cms.untracked.double(0.),
+    flxy               = cms.untracked.double(9999.),
     type               = cms.untracked.int32(1313),
-    maxDoca            = cms.untracked.double(0.08),
     candlo             = cms.untracked.double(2.7),
     candhi             = cms.untracked.double(4.5),
+    extra              = cms.untracked.double(0.3),
+    maxDoca            = cms.untracked.double(0.08),
     pvWeight           = cms.untracked.double(0.6),
     nbrMuons           = cms.untracked.int32(2),
     closeToMuons       = cms.untracked.bool(False)
@@ -25,20 +31,26 @@ upsDump = cms.EDAnalyzer(
     "HFDiTracks",
     verbose            = cms.untracked.int32(0),
     muonsLabel         = cms.untracked.InputTag("muons"),
-    tracksLabel        = cms.untracked.InputTag('generalTracks'),
+    tracksLabel        = cms.untracked.InputTag("generalTracks"),
     PrimaryVertexLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
     BeamSpotLabel      = cms.untracked.InputTag("offlineBeamSpot"),
-    unlikeCharge       = cms.untracked.bool(True),
-    chi2               = cms.untracked.double(5.),
+    muonQualityString  = cms.untracked.string("AllGlobalMuons"),
     muonPt             = cms.untracked.double(4.0),
+    pt                 = cms.untracked.double(5.),
+    chi2               = cms.untracked.double(10.),
+    pvips              = cms.untracked.double(5.),
+    flsxy              = cms.untracked.double(0.),
+    flxy               = cms.untracked.double(9999.),
     type               = cms.untracked.int32(1313),
-    maxDoca            = cms.untracked.double(0.08),
     candlo             = cms.untracked.double(8.0),
     candhi             = cms.untracked.double(12.0),
+    extra              = cms.untracked.double(0.3),
+    maxDoca            = cms.untracked.double(0.08),
     pvWeight           = cms.untracked.double(0.6),
     nbrMuons           = cms.untracked.int32(2),
     closeToMuons       = cms.untracked.bool(False)
     )
+
 
 # ######################################################################
 # Sequences
