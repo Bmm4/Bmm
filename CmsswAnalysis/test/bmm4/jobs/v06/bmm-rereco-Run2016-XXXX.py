f7b0dc175c8caae000a7a9772ae1d9627bdfb9c8
Urs Langenegger <urs.langenegger@psi.ch>
2017-04-06 16:11:10 +0200

    added JSON file filtering

---
+++ CmsswAnalysis/test/bmm4/jobs/v06/bmm-rereco-Run2016-XXXX.py
@@ -0,0 +1,70 @@
+import os
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HFA")
+
+# ----------------------------------------------------------------------
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+
+# ----------------------------------------------------------------------
+# -- Database configuration
+process.load("CondCore.CondDB.CondDB_cfi")
+# -- Conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.Geometry.GeometryDB_cff")
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+
+# requires > CMSSW_8_0_20
+process.GlobalTag.globaltag = "80X_dataRun2_2016SeptRepro_v6"
+
+# ----------------------------------------------------------------------
+# POOLSOURCE
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
+
+
+
+# ----------------------------------------------------------------------
+rootFileName = "bmm-rereco-Run2016-XXXX.root"
+
+process.tree = cms.EDAnalyzer(
+    "HFTree",
+    verbose        = cms.untracked.int32(0),
+    printFrequency = cms.untracked.int32(100),
+    requireCand    =  cms.untracked.bool(True),
+    fullGenBlock   = cms.untracked.bool(False),
+    fileName       = cms.untracked.string(rootFileName)
+    )
+
+
+# ----------------------------------------------------------------------
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Bmm.CmsswAnalysis.HFRecoStuff_cff")
+process.load("Bmm.CmsswAnalysis.HFBmm_cff")
+process.load("Bmm.CmsswAnalysis.HFOnia_cff")
+process.load("Bmm.CmsswAnalysis.HFHadronic_cff")
+process.load("Bmm.CmsswAnalysis.HFPhysicsDeclared_cff")
+process.load("Bmm.CmsswAnalysis.HFSkipEvents_cff")
+
+process.skipEvents.filterOnJson = cms.untracked.int32(1)
+process.skipEvents.JSONFile     = cms.untracked.FileInPath("Bmm/RootAnalysis/common/json/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_MuonPhys.txt")
+
+# ----------------------------------------------------------------------
+process.p = cms.Path(
+    process.skipEvents*
+    process.recoStuffSequence*
+    process.bmmSequence*
+    process.psiDump*
+    process.upsDump*
+    process.dstarDump*
+    process.ksDump*
+    process.phiDump*
+    process.lambdaDump*
+    process.tree
+)
