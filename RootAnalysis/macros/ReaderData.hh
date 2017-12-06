d556b90313177243c9fc80d820705e2d2c2f20a0
Urs Langenegger <urslangenegger@gmail.com>
2017-12-06 12:33:12 +0100

    add flsxy

--- RootAnalysis/macros/ReaderData.hh
+++ RootAnalysis/macros/ReaderData.hh
@@ -3,7 +3,7 @@
 
 struct ReaderData {
   float pt, eta, m1eta, m2eta, m1pt, m2pt, m1phi, m2phi;
-  float fls3d, fl3d, alpha, maxdoca, pvip, pvips, iso, docatrk, chi2dof;
+  float fls3d, fl3d, flsxy, alpha, maxdoca, pvip, pvips, iso, docatrk, chi2dof;
   float closetrk;
   float closetrks1, closetrks2, closetrks3;
   float m1iso, m2iso, pvdchi2, othervtx;
