83a329e0214f5141cfad37b837d064588f3023e9
Urs Langenegger <urslangenegger@gmail.com>
2018-03-27 16:10:29 +0200

    complete clearing

--- RootAnalysis/rootio/TAnaCand.cc
+++ RootAnalysis/rootio/TAnaCand.cc
@@ -15,7 +15,6 @@ TAnaCand::~TAnaCand() {
 
 
 void TAnaCand::clear() {
-  fMass  = -99.;
   fMom   = -99;
   fDau1  = -99;
   fDau2  = -99;
@@ -23,17 +22,28 @@ void TAnaCand::clear() {
   fSig2  = -99;
   fQ     = -99;
   fIndex = -99;
+  fType  = -99;
 
-  fPvIdx = fPv2Idx = -99;
-  fDeltaChi2 = -99;
-  fPvLip = fPvLipE = fPvTip = fPvTipE = -99.;
-  fPv2Lip = fPv2LipE = fPv2Tip = fPv2TipE = -99.;
-  fPvIP3d = fPvIP3dE = fPv2IP3d = fPv2IP3dE = -99.;
-  fTauxy = fTauxyE = -99;
-  fTau3d = fTau3dE = -99.;
-  fMassE = -99.;
+  fMass   = -99.;
+  fMassE  = -99.;
+  fMassC  = -99.;
+  fMassCE = -99.;
 
-  fInt1 = fInt2 = fInt3 -99;
+  fPlab.SetXYZ(-999., -999., -999.);
+  fPtE = fPhiE = fEtaE = -999.;
+  fAlpha = -999.;
+
+  fMinDoca = fMaxDoca = -999.;
+
+  fPvIdx = fPv2Idx = -9999;
+  fPvLip = fPvLipE = fPvTip = fPvTipE = -999.;
+  fPv2Lip = fPv2LipE = fPv2Tip = fPv2TipE = -999.;
+  fPvIP3d = fPvIP3dE = fPv2IP3d = fPv2IP3dE = -999.;
+  fTauxy = fTauxyE = -999.;
+  fTau3d = fTau3dE = -999.;
+  fDeltaChi2 = -9999;
+
+  fInt1 = fInt2 = fInt3 = -99;
   fDouble1 = fDouble2 = fDouble3 = -99.;
 
   fVtx.clear();
@@ -42,7 +52,7 @@ void TAnaCand::clear() {
 
 
 void TAnaCand::dump() {
-  cout << Form("Cand: idx=%3d type=%d m=%5.3f cm=%5.3f, pT=%6.2f f=%+4.3f eta=%+4.3f maxdoca=%4.3f pvips=%4.3f",
+  cout << Form("Cand: idx=%3d type=%d m=%5.3f cm=%5.3f, pT=%6.2f f=%+4.3f eta=%+4.3f maxdoca=%6.5f pvips=%4.3f",
 	       fIndex, fType, fMass, fMassC, fPlab.Perp(), fPlab.Phi(), fPlab.Eta(), fMaxDoca, (fPvIP3dE>0.?fPvIP3d/fPvIP3dE:-1.))
        << endl;
 
