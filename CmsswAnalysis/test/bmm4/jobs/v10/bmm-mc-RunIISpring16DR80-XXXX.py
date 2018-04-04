acc7e65b9b83ee01935e94041962f4768436f3e7
Urs Langenegger <urslangenegger@gmail.com>
2018-04-04 11:19:09 +0200

    migrated to proper MC (truth) candidates for V10

--- CmsswAnalysis/test/bmm4/jobs/v10/bmm-mc-RunIISpring16DR80-XXXX.py
+++ CmsswAnalysis/test/bmm4/jobs/v10/bmm-mc-RunIISpring16DR80-XXXX.py
@@ -58,6 +58,72 @@ process.genDump = cms.EDAnalyzer(
     )
 
 
+process.bmmDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.bupsikpDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.bspsiphiDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.bdpsikstarDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.bspsif0Dump.useBeamspotConstraint = cms.untracked.bool(False)
+
+process.truthBsToMuMuDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBdToMuMuDump.useBeamspotConstraint = cms.untracked.bool(False)
+
+process.truthBsToMuMuGaDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBsToKKDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBsToKPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBsToPiPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBsToPiMuNuDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBsToKMuNuDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBsToPhiMuMuDump.useBeamspotConstraint = cms.untracked.bool(False)
+
+process.truthBdToPiPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBdToKPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBdToKKDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBdToMuMuPi0Dump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBdToPiMuNuDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBdToMuMuGaDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBdToMuMuK0Dump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBdToRhoPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+
+
+process.truthBcToJpsiMuNuDump.useBeamspotConstraint = cms.untracked.bool(False)
+
+process.truthBuTo3MuNuDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBuToMuMuKDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBuToMuMuPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+
+
+process.truthLambdaBToPPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthLambdaBToPKDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthLambdaBToPMuNuDump.useBeamspotConstraint = cms.untracked.bool(False)
+
+process.truthBd2DstarPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+
+process.truthBs2JpsiPhiDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBs2Jpsif0Dump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBs2JpsiPhiAsBpDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBs2JpsiPhiAsBdDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBs2JpsiPiPiAsBsDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBd2JpsiKsDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBd2JpsiKstarDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBd2JpsiKstarAsBpDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBd2JpsiKstarAsBsDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBu2JpsiKpDump.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthBu2JpsiPiDump.useBeamspotConstraint = cms.untracked.bool(False)
+
+process.truthPsiToMuMu.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthPsi2SToMuMu.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthUps1SToMuMu.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthUps2SToMuMu.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthUps3SToMuMu.useBeamspotConstraint = cms.untracked.bool(False)
+
+process.truthD0ToKPi.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthDpToKPiPi.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthDpToKstarPi.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthDsToPhiPi.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthDstarToD0PiToKPiPi.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthDpToKKPi.useBeamspotConstraint = cms.untracked.bool(False)
+process.truthLambdaCToPrKPi.useBeamspotConstraint = cms.untracked.bool(False)
+
 # ----------------------------------------------------------------------
 process.p = cms.Path(
     process.genDump*
