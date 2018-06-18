4e303ea969b0c668bd57d1db3b0cfc64ef3e9fef
Urs Langenegger <urslangenegger@gmail.com>
2018-06-18 15:18:58 +0200

    pacify gcc

--- CmsswAnalysis/plugins/HFDumpTrackJets.cc
+++ CmsswAnalysis/plugins/HFDumpTrackJets.cc
@@ -68,23 +68,23 @@ HFDumpTrackJets::HFDumpTrackJets(const edm::ParameterSet& iConfig):
   fJetsLabel(iConfig.getUntrackedParameter<string>("jetsLabel", string("ic5TrackJets"))),
   fJetsTagLabel(iConfig.getUntrackedParameter<string>("jetsTagLabel", string("simpleSecondaryVertexBJetTags"))),
   fTracksLabel(iConfig.getUntrackedParameter<string>("tracksLabel", string("trackCandidates"))),
-  fGenCandidatesLabel(iConfig.getUntrackedParameter<string>("genparticlesLabel", string("genParticles"))), 
+  fGenCandidatesLabel(iConfig.getUntrackedParameter<string>("genparticlesLabel", string("genParticles"))),
   fsourceByRefer(iConfig.getParameter<edm::InputTag>("sourceByRefer"))
-  
- 
+
+
 {
-  
+
   cout << "----------------------------------------------------------------------" << endl;
   cout << "--- HFDumpTrackJets constructor" << endl;
   cout << "--- " << fJetsLabel.c_str() << endl;
   cout << "----------------------------------------------------------------------" << endl;
-  
+
 }
 
 
 // ----------------------------------------------------------------------
 HFDumpTrackJets::~HFDumpTrackJets() {
-  
+
 }
 
 
@@ -93,12 +93,14 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
   if(fVerbose > 0) cout<<" ===HFDumpTrackJets "<<endl;
   nevt++;
 
-  // handle to 0.5 cone ctf track jets  
+  // handle to 0.5 cone ctf track jets
   edm::Handle<BasicJetCollection> jetsH;
   iEvent.getByLabel(fJetsLabel.c_str(),jetsH);
-  const BasicJetCollection *jets   = jetsH.product(); 
+  const BasicJetCollection *jets   = jetsH.product();
   if( !jetsH.isValid()) {
-    if (fVerbose > 0) cout << "**** HFDumpTrackJets>  no " << fJetsLabel << endl; return;}
+    if (fVerbose > 0) cout << "**** HFDumpTrackJets>  no " << fJetsLabel << endl;
+    return;
+  }
 
   // get btag
   // Get b tag information
@@ -115,27 +117,27 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 
   //tracks (jet constituents)
   Handle<reco::CandidateView> candidates1Handle;
-  iEvent.getByLabel(fTracksLabel.c_str(), candidates1Handle); 
+  iEvent.getByLabel(fTracksLabel.c_str(), candidates1Handle);
 
   int jetIndex=0;
   bool first = true;
-  TAnaJet *pTrackJet; 
+  TAnaJet *pTrackJet;
 
   JetMatchedPartonsCollection::const_iterator j;
   for ( BasicJetCollection::const_iterator it = jets->begin(); it != jets->end(); it ++ ) {
- 
+
     //for(pat::JetCollection::const_iterator it = jetsH->begin(); it != jetsH->end(); ++it) {
 
 
-   
+
     pTrackJet = gHFEvent->addTrackJet();
-    pTrackJet->fIndex            = jetIndex;  
+    pTrackJet->fIndex            = jetIndex;
 
     pTrackJet->fPlab.SetPtEtaPhi(it->pt(),
 				 it->eta(),
 				 it->phi()
 				 );
- 
+
     pTrackJet->fQ                = it->charge();
     pTrackJet->fE                = it->energy();
     pTrackJet->fEt               = it->et();
@@ -148,23 +150,23 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 
     pTrackJet->fn60              = -9999;//not used
     pTrackJet->fn90              = -9999;//not used
-    
+
     pTrackJet->fJetFlavorAlgo    = -9999;
     pTrackJet->fJetFlavorPhys    = -9999;
     pTrackJet->fJetFlavorEne     = -9999;
-  
+
     pTrackJet->fD1               = -9999; //
     pTrackJet->fD2               = -9999; //fJetFlavorNear2
     pTrackJet->fD3               = -9999; //fJetFlavorNear3
     pTrackJet->fD4               = -9999; //fJetFlavorHeavy
     pTrackJet->fD5               = -9999; //not used
     pTrackJet->fD6               = -9999; //not used
-    pTrackJet->fD7               = -9999; //not used 
+    pTrackJet->fD7               = -9999; //not used
 
     // not working...
     //    pTrackJet->ftrjpvindx            = it->primaryVertex().index(); //TrackJet primary vertex index (O-first from HI)
     pTrackJet->fbtag         = -9999; // btag SSV output
-   
+
 
     //    pTrackJet->fVtx.fPoint.SetX(it->primaryVertex().x());
     //    pTrackJet->fVtx.fPoint.SetY(it->primaryVertex().y());
@@ -180,7 +182,7 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 	   ijt != tagColl.end(); ++ijt) {
 	// match with actual jet
 	TVector3 jetbcand;
-	jetbcand.SetPtEtaPhi(ijt->first->pt(), ijt->first->eta(), ijt->first->phi());  
+	jetbcand.SetPtEtaPhi(ijt->first->pt(), ijt->first->eta(), ijt->first->phi());
 	double r = (pTrackJet->fPlab).DeltaR(jetbcand);
 	if (fVerbose > 1) cout<<" btag match "<<r<<endl;
 	if(r<rmin) pTrackJet->fbtag= ijt->second;
@@ -199,29 +201,29 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 	for (unsigned int j = 0; j < candidates1Handle->size(); ++ j ) {
 	  const Candidate &p2 = (*candidates1Handle)[j];
 	  const Candidate *  p1 = &p2;
-	  if (consti->pt() == p1->pt() && consti->phi() == p1->phi() && consti->eta() == p1->eta() ) 
+	  if (consti->pt() == p1->pt() && consti->phi() == p1->phi() && consti->eta() == p1->eta() )
 	    index = j;
-	  
+
 	}
 	pTrackJet->addTrack(index);
 	if (fVerbose) cout << index << " pt " << consti->pt() << " phi " << consti->phi() << " eta " << consti->eta() << endl;
-      } 
- 
-    } 
+      }
 
-    if (fDoFlavorTagging == 1) { 
-    
+    }
+
+    if (fDoFlavorTagging == 1) {
+
 
       //////////////////////////////////////////
       // -- get the collection for flavor matching
       edm::Handle<reco::JetMatchedPartonsCollection> theTagByRef;
       iEvent.getByLabel (fsourceByRefer , theTagByRef);
-      
-      // -- get the collection of GenParticles 
+
+      // -- get the collection of GenParticles
       edm::Handle<GenParticleCollection> genParticlesH;
       genParticlesH.clear();
       iEvent.getByLabel (fGenCandidatesLabel.c_str(), genParticlesH);
-      
+
       //GenParticles for Jet Flavor Tagging
       std::vector<const GenParticle *> cands;
       cands.clear();
@@ -229,11 +231,11 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
       for (GenParticleCollection::const_iterator p = genParticlesH->begin(); p != genParticlesH->end(); ++p) {
 	cands.push_back( & * p );
       }
-      
+
       if (jetsH->size() != theTagByRef->size()) {
-	if (fVerbose > 0) cout << "==>HFDumpTrackJets>ERROR: Different Size of JetCollections " << endl; 
+	if (fVerbose > 0) cout << "==>HFDumpTrackJets>ERROR: Different Size of JetCollections " << endl;
       }
-      
+
       ///////////////////////////////////////////
 
       if (first) {
@@ -242,12 +244,12 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
       }
 
       const Jet *aJet             = (*j).first.get();
-      const MatchedPartons aMatch = (*j).second; 
-      
+      const MatchedPartons aMatch = (*j).second;
+
       if (it->eta() != aJet->eta()) {
-	if (fVerbose > 0) cout << "==>HFDumpTrackJets>ERROR: Different jets in JetCollections " << endl; 
+	if (fVerbose > 0) cout << "==>HFDumpTrackJets>ERROR: Different jets in JetCollections " << endl;
       }
-      
+
       //jet tag
       const GenParticleRef theHeaviest = aMatch.heaviest() ;
       if(theHeaviest.isNonnull()) {
@@ -256,14 +258,14 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 	if (found != cands.end()) {
 	  index = found - cands.begin();
 	  pTrackJet->fD4 = index;
-	} 
+	}
 	if (fVerbose > 0) {
-	  cout << "theHeaviest flav idx (p,eta,phi)= " 
-	       << theHeaviest.get()->pdgId() << " " 
+	  cout << "theHeaviest flav idx (p,eta,phi)= "
+	       << theHeaviest.get()->pdgId() << " "
 	       << index
-	       << " (" << theHeaviest.get()->p() 
-	       << ","  << theHeaviest.get()->eta() 
-	       << ","  << theHeaviest.get()->phi() << ") " << endl; 
+	       << " (" << theHeaviest.get()->p()
+	       << ","  << theHeaviest.get()->eta()
+	       << ","  << theHeaviest.get()->phi() << ") " << endl;
 	}
       }
       const GenParticleRef theNearest2 = aMatch.nearest_status2() ;
@@ -275,12 +277,12 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 	  pTrackJet->fD2 = index;
 	}
 	if (fVerbose > 0) {
-	  cout << "theNearest Stat2  flav idx (p,eta,phi)= " 
-	       << theNearest2.get()->pdgId() << " " 
+	  cout << "theNearest Stat2  flav idx (p,eta,phi)= "
+	       << theNearest2.get()->pdgId() << " "
 	       << index
-	       << " (" << theNearest2.get()->p() 
-	       << ","  << theNearest2.get()->eta() 
-	       << ","  << theNearest2.get()->phi() << ") " << endl; 
+	       << " (" << theNearest2.get()->p()
+	       << ","  << theNearest2.get()->eta()
+	       << ","  << theNearest2.get()->phi() << ") " << endl;
 	}
       }
       const GenParticleRef theNearest3 = aMatch.nearest_status3() ;
@@ -292,12 +294,12 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 	  pTrackJet->fD3 = index;
 	}
 	if (fVerbose > 0) {
-	  cout << "theNearest Stat3  flav idx (p,eta,phi)= " 
-	       << theNearest3.get()->pdgId() << " " 
+	  cout << "theNearest Stat3  flav idx (p,eta,phi)= "
+	       << theNearest3.get()->pdgId() << " "
 	       << index
-	       << " (" << theNearest3.get()->p() 
-	       << ","  << theNearest3.get()->eta() 
-	       << ","  << theNearest3.get()->phi() << ") " << endl; 
+	       << " (" << theNearest3.get()->p()
+	       << ","  << theNearest3.get()->eta()
+	       << ","  << theNearest3.get()->phi() << ") " << endl;
 	}
       }
       const GenParticleRef thePhyDef = aMatch.physicsDefinitionParton() ;
@@ -309,14 +311,14 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 	  pTrackJet->fJetFlavorPhys = index;
 	}
 	if (fVerbose > 0) {
-	  cout << "thePhysDefinition flav idx (p,eta,phi)= " 
-	       << thePhyDef.get()->pdgId() << " " 
-	       << index 
-	       << " (" << thePhyDef.get()->p() 
-	       << ","  << thePhyDef.get()->eta() 
-	       << ","  << thePhyDef.get()->phi() << ") " << endl; 
+	  cout << "thePhysDefinition flav idx (p,eta,phi)= "
+	       << thePhyDef.get()->pdgId() << " "
+	       << index
+	       << " (" << thePhyDef.get()->p()
+	       << ","  << thePhyDef.get()->eta()
+	       << ","  << thePhyDef.get()->phi() << ") " << endl;
 	}
-	
+
       }
       const GenParticleRef theAlgDef = aMatch.algoDefinitionParton() ;
       if(theAlgDef.isNonnull()) {
@@ -327,17 +329,17 @@ void HFDumpTrackJets::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 	  pTrackJet->fJetFlavorAlgo = index;
 	}
 	if (fVerbose > 0) {
-	  cout << "theAlgoDefinition flav idx (p,eta,phi)= " 
-	       << theAlgDef.get()->pdgId() << " " 
-	       << index 
-	       << " (" << theAlgDef.get()->p() 
-	       << ","  << theAlgDef.get()->eta() 
-	       << ","  << theAlgDef.get()->phi() << ") " << endl; 
-	} 
-	
+	  cout << "theAlgoDefinition flav idx (p,eta,phi)= "
+	       << theAlgDef.get()->pdgId() << " "
+	       << index
+	       << " (" << theAlgDef.get()->p()
+	       << ","  << theAlgDef.get()->eta()
+	       << ","  << theAlgDef.get()->phi() << ") " << endl;
+	}
+
       }
-      
-    } 
+
+    }
     if (fVerbose > 0) pTrackJet->dump();
     jetIndex++;
     j++;
@@ -351,9 +353,9 @@ void  HFDumpTrackJets::beginJob() {
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
-void  HFDumpTrackJets::endJob() { 
+void  HFDumpTrackJets::endJob() {
   cout << "HFDumpTrackJet>   Summary: Events processed: " << nevt << endl;
-   
+
 }
 
 //define this as a plug-in
