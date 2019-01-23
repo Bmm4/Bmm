21fa677c70e38f360bc84cacc37545ec3086f48d
Urs Langenegger <urslangenegger@gmail.com>
2019-01-23 17:09:15 +0100

    add different()

--- CmsswAnalysis/interface/HFThreeParticleCombinatorics.hh
+++ CmsswAnalysis/interface/HFThreeParticleCombinatorics.hh
@@ -15,45 +15,56 @@ public:
      } else {
 	   pion1=c; pion2=b;}
   };
-  
+
   bool operator==(triplet t) {return (kaon==t.ka() && pion1==t.pi1() && pion2==t.pi2()); };
-  unsigned int pi1(){ return pion1;};
-  unsigned int pi2(){ return pion2;};
-  unsigned int ka(){ return kaon;};
-  unsigned int pi3(){ return kaon;};
+  bool different(triplet a){
+    if (pion1 == a.kaon) return false;
+    if (pion1 == a.pion1) return false;
+    if (pion1 == a.pion2) return false;
+    if (pion2 == a.kaon) return false;
+    if (pion2 == a.pion1) return false;
+    if (pion2 == a.pion2) return false;
+    if (kaon == a.kaon) return false;
+    if (kaon == a.pion1) return false;
+    if (kaon == a.pion2) return false;
+    return true;
+  };
+  int pi1(){ return pion1;};
+  int pi2(){ return pion2;};
+  int ka(){ return kaon;};
+  int pi3(){ return kaon;};
 
 private:
-   unsigned int kaon; 
-   unsigned int pion1; 
-   unsigned int pion2;
+   int kaon;
+   int pion1;
+   int pion2;
 };
 
 // ----------------------------------------------------------------------
 class HFThreeParticleCombinatorics {
-  
+
 public:
-  
+
   HFThreeParticleCombinatorics(int verbose = 0);
   ~HFThreeParticleCombinatorics();
 
-// combining particle from KList with two particles from piList into a combination 
+// combining particle from KList with two particles from piList into a combination
 // with mass between loMass and hiMass
 //
-  void combine(std::vector<triplet > &combList, 
-	       std::vector<std::pair<int, TLorentzVector> > &KList, 
-	       std::vector<std::pair<int, TLorentzVector> > &piList, 
+  void combine(std::vector<triplet > &combList,
+	       std::vector<std::pair<int, TLorentzVector> > &KList,
+	       std::vector<std::pair<int, TLorentzVector> > &piList,
 	       double loMass = 0.2, double hiMass = 0.8);
 
 // combine three particles from piList into a combination with mass between loMass and hiMass
-// if loResMass and hiResMass >0, the first two particles form a resonance 
+// if loResMass and hiResMass >0, the first two particles form a resonance
 // with mass between loResMass and hiResMass
 //
-  void combine(std::vector<triplet > &combList, 
-	       std::vector<std::pair<int, TLorentzVector> > &piList, 
+  void combine(std::vector<triplet > &combList,
+	       std::vector<std::pair<int, TLorentzVector> > &piList,
 	       double loMass = 0.4, double hiMass = 20., double loResMass = 0., double hiResMass = 0.);
 
-  int fVerbose; 
+  int fVerbose;
 };
 
 #endif
-
--- CmsswAnalysis/src/HFThreeParticleCombinatorics.cc
+++ CmsswAnalysis/src/HFThreeParticleCombinatorics.cc
@@ -9,13 +9,12 @@ using std::pair;
 
 
 // ----------------------------------------------------------------------
-HFThreeParticleCombinatorics::HFThreeParticleCombinatorics(int verbose) {  
+HFThreeParticleCombinatorics::HFThreeParticleCombinatorics(int verbose) {
   fVerbose  = verbose;
 }
 
 // ----------------------------------------------------------------------
 HFThreeParticleCombinatorics::~HFThreeParticleCombinatorics() {
-  if (fVerbose > 2) cout << "This is the end" << endl;
 }
 
 
@@ -26,33 +25,33 @@ HFThreeParticleCombinatorics::~HFThreeParticleCombinatorics() {
 // E.g.: D+ -> K pi pi
 // First is Kaon, second and third are pions with ordered indices
 // ----------------------------------------------------------------------
-void HFThreeParticleCombinatorics::combine(vector<triplet> &combList, 
-					 vector<pair<int, TLorentzVector> > &kaList, 
-					 vector<pair<int, TLorentzVector> > &piList, 
+void HFThreeParticleCombinatorics::combine(vector<triplet> &combList,
+					 vector<pair<int, TLorentzVector> > &kaList,
+					 vector<pair<int, TLorentzVector> > &piList,
 					 double loMass, double hiMass) {
 
   if (fVerbose > 2) {
     cout << "kaList: " << kaList.size() << endl;
     for (unsigned int i = 0; i < kaList.size(); ++i) {
-      cout << "i = " << i << " index = " << kaList[i].first 
-	   << " pT = " << kaList[i].second.Pt() 
-	   << " phi = " << kaList[i].second.Phi() 
-	   << " eta = " << kaList[i].second.Eta() 
+      cout << "i = " << i << " index = " << kaList[i].first
+	   << " pT = " << kaList[i].second.Pt()
+	   << " phi = " << kaList[i].second.Phi()
+	   << " eta = " << kaList[i].second.Eta()
 	   << endl;
     }
     cout << "piList: " << piList.size() << endl;
     for (unsigned int i = 0; i < piList.size(); ++i) {
-      cout << "i = " << i << " index = " << piList[i].first 
-	   << " pT = " << piList[i].second.Pt() 
-	   << " phi = " << piList[i].second.Phi() 
-	   << " eta = " << piList[i].second.Eta() 
+      cout << "i = " << i << " index = " << piList[i].first
+	   << " pT = " << piList[i].second.Pt()
+	   << " phi = " << piList[i].second.Phi()
+	   << " eta = " << piList[i].second.Eta()
 	   << endl;
     }
   }
-  
-  TLorentzVector mom; 
-  double mass(0.); 
-  int duplicate(0); 
+
+  TLorentzVector mom;
+  double mass(0.);
+  int duplicate(0);
   vector<pair<int, TLorentzVector> >::iterator ka, pi1, pi2;
   for (ka = kaList.begin(); ka != kaList.end(); ka++) {                 // Kaon
     for (pi1 = piList.begin(); pi1 != piList.end(); pi1++) {            // first pion
@@ -61,17 +60,17 @@ void HFThreeParticleCombinatorics::combine(vector<triplet> &combList,
          if (pi2->first == ka->first) continue;                         // don't use the same track twice
 	 triplet t=triplet(ka->first, pi1->first, pi2->first);          // triplet with three different tracks
          mom = ka->second +  pi1->second + pi2->second;                 // mother is D+ candidate
-         mass = mom.M(); 
+         mass = mom.M();
          if (loMass < mass && mass < hiMass) {                          // continue only when in mass window
-	   duplicate = 0;                                               // check for duplication 
+	   duplicate = 0;                                               // check for duplication
 	    for (vector<triplet>::iterator it=combList.begin(); it != combList.end(); it++) {
 	       if(t==*it){
-	          duplicate = 1; 
+	          duplicate = 1;
 	          break;
 	       }
 	    }
 	    if (0 == duplicate) combList.push_back(t);                   // add only if not already in the list
-         } 
+         }
       }
     }
   }
@@ -95,29 +94,29 @@ void HFThreeParticleCombinatorics::combine(vector<triplet> &combList,
 // E.g.: Kshort pi --> 3 pi
 // First two pions are from Kshort candidate, third comes from D+ vertex
 // ----------------------------------------------------------------------
-void HFThreeParticleCombinatorics::combine(vector<triplet> &combList, 
-					 vector<pair<int, TLorentzVector> > &piList, 
+void HFThreeParticleCombinatorics::combine(vector<triplet> &combList,
+					 vector<pair<int, TLorentzVector> > &piList,
 					   double loMass, double hiMass, double loResMass, double hiResMass) {
 
   if (fVerbose > 2) {
     cout << "piList: " << piList.size() << endl;
     for (unsigned int i = 0; i < piList.size(); ++i) {
-      cout << "i = " << i << " index = " << piList[i].first 
-	   << " pT = " << piList[i].second.Pt() 
-	   << " phi = " << piList[i].second.Phi() 
-	   << " eta = " << piList[i].second.Eta() 
+      cout << "i = " << i << " index = " << piList[i].first
+	   << " pT = " << piList[i].second.Pt()
+	   << " phi = " << piList[i].second.Phi()
+	   << " eta = " << piList[i].second.Eta()
 	   << endl;
     }
   }
 
-  TLorentzVector mom; 
-  double mass(0.); 
-  int duplicate(0); 
+  TLorentzVector mom;
+  double mass(0.);
+  int duplicate(0);
   vector<pair<int, TLorentzVector> >::iterator i,j,k;
   for (i = piList.begin(); i != piList.end(); ++i) {                         // first pion from Kshort
     for (j = i+1; j != piList.end(); ++j) {                                  // second pion from Kshort
       mom = i->second +  j->second;                                          // Resonance candidate
-      mass = mom.M(); 
+      mass = mom.M();
       if(hiResMass>0. && (loResMass > mass || mass > hiResMass)) continue;   // ignore if resonance outside mass window
       for(k = piList.begin(); k!=piList.end(); k++) {                        // third pion, from D+
 	if (k==i || k==j) continue;                                          // don't use same track twice
@@ -125,10 +124,10 @@ void HFThreeParticleCombinatorics::combine(vector<triplet> &combList,
         mass = mom.M();
         if(loMass > mass || mass > hiMass) continue;                        // ignore if outside mass window
 	triplet t=triplet(k->first, i->first, j->first);                     // triplet with three different tracks
-	duplicate = 0;                                                       // check for duplication                 
+	duplicate = 0;                                                       // check for duplication
 	for(vector<triplet>::iterator it=combList.begin(); it != combList.end(); it++) {
 	    if(t==*it){
-	       duplicate = 1; 
+	       duplicate = 1;
 	       break;
 	    }
 	}
