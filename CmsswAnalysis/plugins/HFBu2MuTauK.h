b092e5e28d238157101a22114f8e9a917d6df5f4
Urs Langenegger <urslangenegger@gmail.com>
2019-04-04 17:21:55 +0200

    add more cuts

--- CmsswAnalysis/plugins/HFBu2MuTauK.h
+++ CmsswAnalysis/plugins/HFBu2MuTauK.h
@@ -4,7 +4,14 @@
 #include "Bmm/CmsswAnalysis/plugins/HFVirtualDecay.h"
 
 #include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/GeometryCommonDetAlgo/interface/Measurement1D.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+
 #include "RecoVertex/KinematicFitPrimitives/interface/RefCountedKinematicTree.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
 
 #include "Bmm/RootAnalysis/rootio/TGenCand.hh"
 
@@ -21,13 +28,16 @@ class HFBu2MuTauK : public HFVirtualDecay {
   // -- produce a vector of int that matches the list of truth-identified tracks
   std::vector<int> truthIndices();
   // -- starting from a vector of int (mu, ka, had1, had2, had3) build a candidate of type
-  bool buildCandidate(std::vector<int>, int type, bool applyCuts = true);
+  TAnaCand* buildCandidate(std::vector<int>, int type, bool applyCuts = true);
   // -- write a candidate to the tree (if all cuts are passing)
-  TAnaCand* fillCand(int type, RefCountedKinematicTree &tree, std::vector<int> trkIdx, reco::Vertex &vtx);
-
+  TAnaCand* fillCand(int type, RefCountedKinematicTree &tree, std::vector<int> trkIdx, std::vector<int> trkId, reco::Vertex &vtx);
+  // -- given something, calculate IP(3D) wrt a vertex
+  std::pair<bool, Measurement1D> doca2Vtx(reco::TransientTrack &, reco::Vertex &);
+  std::pair<bool, Measurement1D> doca2Vtx(RefCountedKinematicTree &, reco::Vertex &);
+  std::pair<bool, Measurement1D> doca2Vtx(TrajectoryStateOnSurface &, reco::Vertex &);
   int    fMcType;
-  double fMuKaVtxProb;
-  double fTauVtxProb;
+  double fMuKaVtxProb, fMuKaMaxDoca;
+  double fTauVtxProb, fTauMaxDoca, fTauMaxIP;
 
   TGenCand *fpGenB, *fpGenMu, *fpGenKa, *fpGenTau, *fpGenHad1, *fpGenHad2, *fpGenHad3;
 
